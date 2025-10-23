PalyrimOS/system_core

debuggerd: Adapt rom name in tombstones
Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

debuggerd/libdebuggerd/tombstone_proto_to_text.cpp
@@ -474,7 +474,7 @@ void print_logs(CallbackType callback, const Tombstone& tombstone, int tail) {
 
 bool tombstone_proto_to_text(const Tombstone& tombstone, CallbackType callback) {
   CBL("*** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***");
-  CBL("LineageOS Version: '%s'", tombstone.lineage_version().c_str());
+  CBL("PalyrimOS Version: '%s'", tombstone.lineage_version().c_str());
   CBL("Build fingerprint: '%s'", tombstone.build_fingerprint().c_str());
   CBL("Revision: '%s'", tombstone.revision().c_str());
   CBL("ABI: '%s'", abi_string(tombstone));

crdroidandroid/android_system_core

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

debuggerd/libdebuggerd/tombstone_proto_to_text.cpp
@@ -580,7 +580,7 @@ static void print_guest_thread(CallbackType callback, SymbolizeCallbackType symb
 bool tombstone_proto_to_text(const Tombstone& tombstone, CallbackType callback,
                              SymbolizeCallbackType symbolize) {
   CBL("*** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***");
-  CBL("LineageOS Version: '%s'", tombstone.lineage_version().c_str());
+  CBL("crDroid Version: '%s'", tombstone.lineage_version().c_str());
   CBL("Build fingerprint: '%s'", tombstone.build_fingerprint().c_str());
   CBL("Revision: '%s'", tombstone.revision().c_str());
   CBL("ABI: '%s'", abi_string(tombstone.arch()));